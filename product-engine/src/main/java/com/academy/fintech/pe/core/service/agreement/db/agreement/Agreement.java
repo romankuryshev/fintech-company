925fde21e37a6786d8989d318fd036b9ad7e1a3a
romankuryshev <romankuryshev1406@gmail.com>
2023-11-20 17:42:31 +0300

    feat: add data layer for Agreement

--- product-engine/src/main/java/com/academy/fintech/pe/core/service/agreement/db/agreement/Agreement.java
+++ product-engine/src/main/java/com/academy/fintech/pe/core/service/agreement/db/agreement/Agreement.java
@@ -1,17 +1,62 @@
 package com.academy.fintech.pe.core.service.agreement.db.agreement;
 
+import com.academy.fintech.pe.core.service.agreement.db.product.Product;
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
-@Data
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
 @Entity
+@Getter
+@Setter
+@Builder
+@Table(name = "agreement")
 @NoArgsConstructor
+@AllArgsConstructor
 public class Agreement {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int agreementId;
 
-    @Enumerated(EnumType.STRING)
-    private AgreementType agreementTypeId;
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "product_code", referencedColumnName = "code")
+    private Product product;
+
+    @NotNull
+    @Column(name = "client_id")
+    private UUID clientId;
+
+    @NotNull
+    @Column(name = "interest")
+    private BigDecimal interest;
+
+    @NotNull
+    @Column(name = "term")
+    private int term;
+
+    @NotNull
+    @Column(name = "principal_amount")
+    private BigDecimal principalAmount;
+
+    @NotNull
+    @Column(name = "origination_amount")
+    private BigDecimal originationAmount;
+
+    @NotNull
+    @Column(name = "status")
+    @Enumerated(value = EnumType.STRING)
+    private AgreementStatus status;
+
+    @Column(name = "disbursement_date")
+    private LocalDate disbursementDate;
+
+    @Column(name = "next_payment_date")
+    private LocalDate nextPaymentDate;
 }
