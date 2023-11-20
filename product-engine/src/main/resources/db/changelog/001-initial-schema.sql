d81ff3f3856bd19f5282b0423e0614f62f183d38
romankuryshev <romankuryshev1406@gmail.com>
2023-11-20 17:39:46 +0300

    feat: add migration
    Create db creation script
    Create insert first product script

--- product-engine/src/main/resources/db/changelog/001-initial-schema.sql
+++ product-engine/src/main/resources/db/changelog/001-initial-schema.sql
@@ -1,108 +1,56 @@
-CREATE TABLE user_info
+CREATE TABLE product
 (
-    user_id    int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    first_name varchar(30) not null,
-    last_name  varchar(30) not null,
-    email      varchar(30) not null,
-    income     numeric     not null
-);
-
-CREATE TABLE agreement_type
-(
-    agreement_type_id   int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    agreement_type_name varchar(30) not null
+    code                   varchar(30) not null PRIMARY KEY,
+    min_term               smallint    not null,
+    max_term               smallint    not null,
+    min_interest           numeric     not null,
+    max_interest           numeric     not null,
+    min_principal_amount   numeric     not null,
+    max_principal_amount   numeric     not null,
+    min_origination_amount numeric     not null,
+    max_origination_amount numeric     not null
 );
 
 CREATE TABLE agreement
 (
-    agreement_id      int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    agreement_type_id int not null,
+    id                 uuid        not null PRIMARY KEY,
+    product_code       varchar(30) not null,
+    client_id          uuid        not null,
+    interest           numeric     not null,
+    term               serial      not null,
+    principal_amount   numeric     not null,
+    origination_amount numeric     not null,
+    status             varchar(30) not null check (status in ('NEW', 'ACTIVE', 'CLOSED')),
+    disbursement_date  date,
+    next_payment_date  date,
 
-    CONSTRAINT agreement_type_fk FOREIGN KEY (agreement_type_id)
-        REFERENCES agreement_type (agreement_type_id)
+    CONSTRAINT product_code_fk FOREIGN KEY (product_code)
+        REFERENCES product (code)
 );
 
-CREATE TABLE cash_loan
+CREATE TABLE payment_schedule
 (
-    name                   varchar(20) not null,
-    version                varchar(10) not null,
-    min_term               smallint    not null,
-    max_term               smallint    not null,
-    min_interest           real        not null,
-    max_interest           real        not null,
-    min_principal_amount   numeric     not null,
-    max_principal_amount   numeric     not null,
-    min_origination_amount numeric     not null,
-    max_origination_amount numeric     not null,
-
-    CONSTRAINT cash_loan_pk PRIMARY KEY (name, version)
-);
-
-CREATE TABLE loan_payment_status
-(
-    payment_status_id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name              varchar(30)
-);
-
-CREATE TABLE cash_loan_status
-(
-    cash_loan_status_id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name                varchar(30)
-);
-
-CREATE TABLE cash_loan_agreement
-(
-    agreement_id                  int PRIMARY KEY,
-    cash_loan_name                varchar(20) not null,
-    cash_loan_version             varchar(10) not null,
-    user_id                       int         not null,
-    agreement_number              int         not null UNIQUE,
-    term                          int         not null,
-    status_id                     int         not null,
-    loan_payment_schedule_version int         not null,
-    interest                      real        not null,
-    principal_amount              numeric     not null,
-    origination_amount            numeric     not null,
-    disbursement_amount           numeric     not null,
-    disbursement_date             date        not null,
+    id           bigserial not null PRIMARY KEY,
+    agreement_id uuid      not null,
+    version      int       not null,
 
     CONSTRAINT agreement_fk FOREIGN KEY (agreement_id)
-        REFERENCES agreement (agreement_id),
+        REFERENCES agreement (id),
 
-    CONSTRAINT user_fk FOREIGN KEY (user_id)
-        REFERENCES user_info (user_id),
-
-    CONSTRAINT status_fk FOREIGN KEY (status_id)
-        REFERENCES cash_loan_status (cash_loan_status_id),
-
-    CONSTRAINT cash_loan_fk FOREIGN KEY (cash_loan_name, cash_loan_version)
-        REFERENCES cash_loan (name, version)
+    CONSTRAINT unique_schedule UNIQUE (agreement_id, version)
 );
 
-CREATE TABLE loan_payment_schedule
+CREATE TABLE payment_schedule_payment
 (
-    version      int not null,
-    agreement_id int not null,
+    paymentId           bigserial   not null PRIMARY KEY,
+    payment_schedule_id bigserial   not null,
+    status              varchar(30) not null check ( status in ('PAID', 'OVERDUE', 'FUTURE')),
+    payment_date        date        not null,
+    period_payment      numeric     not null,
+    interest_payment    numeric     not null,
+    principal_payment   numeric     not null,
+    period_number       int         not null,
 
-    CONSTRAINT loam_payment_schedule_pk PRIMARY KEY (version, agreement_id),
-
-    CONSTRAINT cash_loan_agreement_fk FOREIGN KEY (agreement_id)
-        REFERENCES cash_loan_agreement (agreement_id)
-);
-
-
-CREATE TABLE loan_payment
-(
-    loan_payment_id               int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    loan_payment_schedule_version int     not null,
-    agreement_id                  int     not null,
-    status_id                     int     not null,
-    payment_date                  date    not null,
-    amount                        numeric not null,
-
-    CONSTRAINT status_fk FOREIGN KEY (status_id)
-        REFERENCES loan_payment_status (payment_status_id),
-
-    CONSTRAINT loan_payment_schedule_fk FOREIGN KEY (loan_payment_schedule_version, agreement_id)
-        REFERENCES loan_payment_schedule (version, agreement_id)
+    CONSTRAINT payment_schedule_pk FOREIGN KEY (payment_schedule_id)
+        REFERENCES payment_schedule (id)
 );
---
+++ product-engine/src/main/resources/db/changelog/003-alter-payment-schedule-payment.sql
@@ -0,0 +1,2 @@
+ALTER TABLE payment_schedule_payment
+RENAME COLUMN paymentid TO payment_id;
