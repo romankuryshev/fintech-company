08b8d509ab56d35f315dfb330f3f67e2f3b0723a
romankuryshev <romankuryshev1406@gmail.com>
2023-11-20 17:43:11 +0300

    feat: add data layer for Product

---
+++ product-engine/src/main/java/com/academy/fintech/pe/core/service/agreement/db/product/Product.java
@@ -0,0 +1,53 @@
+package com.academy.fintech.pe.core.service.agreement.db.product;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @NotNull
+    @Column(name = "code")
+    private String code;
+
+    @NotNull
+    @Column(name = "min_term")
+    private int minTerm;
+
+    @NotNull
+    @Column(name = "max_term")
+    private int maxTerm;
+
+    @NotNull
+    @Column(name = "min_principal_amount")
+    private BigDecimal minPrincipalAmount;
+
+    @NotNull
+    @Column(name = "max_principal_amount")
+    private BigDecimal maxPrincipalAmount;
+
+    @NotNull
+    @Column(name = "min_interest")
+    private BigDecimal minInterest;
+
+    @NotNull
+    @Column(name = "max_interest")
+    private BigDecimal maxInterest;
+
+    @NotNull
+    @Column(name = "min_origination_amount")
+    private BigDecimal minOriginationAmount;
+
+    @NotNull
+    @Column(name = "max_origination_amount")
+    private BigDecimal maxOriginationAmount;
+}
---
+++ product-engine/src/main/java/com/academy/fintech/pe/core/service/agreement/db/product/ProductService.java
@@ -0,0 +1,18 @@
+package com.academy.fintech.pe.core.service.agreement.db.product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product getProduct(String productCode) {
+        return productRepository.findByCode(productCode);
+    }
+}
