89247e8be269fd8d7e06a171181c3923524791fa
romankuryshev <romankuryshev1406@gmail.com>
2023-11-28 21:51:02 +0300

    fix: replace package imports

--- origination/src/main/java/com/academy/fintech/origination/core/service/application/db/application/Application.java
+++ origination/src/main/java/com/academy/fintech/origination/core/service/application/db/application/Application.java
@@ -2,8 +2,22 @@ package com.academy.fintech.origination.core.service.application.db.application;
 
 
 import com.academy.fintech.origination.core.service.application.db.client.Client;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.util.UUID;
