9cd7a33bd1ecc3a5738db9d2166afbd1b8154285
romankuryshev <romankuryshev1406@gmail.com>
2023-12-05 00:21:36 +0300

    feat: add email notification

--- origination/src/main/java/com/academy/fintech/origination/core/service/application/application_processing/AsyncApplicationProcessor.java
+++ origination/src/main/java/com/academy/fintech/origination/core/service/application/application_processing/AsyncApplicationProcessor.java
@@ -1,5 +1,6 @@
 package com.academy.fintech.origination.core.service.application.application_processing;
 
+import com.academy.fintech.origination.core.email.EmailClientService;
 import com.academy.fintech.origination.core.scoring.client.ScoringClientService;
 import com.academy.fintech.origination.core.service.application.db.application.Application;
 import com.academy.fintech.origination.core.service.application.db.application.ApplicationService;
@@ -14,12 +15,16 @@ public class AsyncApplicationProcessor {
 
     private final ApplicationService applicationService;
 
+    private final EmailClientService emailClientService;
+
     private final ScoringClientService scoringClientService;
 
     @Async
     public void processApplication(Application application) {
         ApplicationStatus status = scoringClientService.processApplication(application);
         application.setStatus(status);
+
         applicationService.save(application);
+        emailClientService.sendApplicationStatusNotification(application);
     }
 }
