604cef82e98865a29150e32a2d2bd3bc830d2627
romankuryshev <romankuryshev1406@gmail.com>
2023-12-05 09:14:40 +0300

    fix: refactor email notification

--- origination/src/main/java/com/academy/fintech/origination/core/email/EmailClientService.java
+++ origination/src/main/java/com/academy/fintech/origination/core/email/EmailClientService.java
@@ -1,34 +1,8 @@
 package com.academy.fintech.origination.core.email;
 
 import com.academy.fintech.origination.core.service.application.db.application.Application;
-import com.academy.fintech.origination.core.service.application.db.client.Client;
-import lombok.AllArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.stereotype.Service;
 
-@Service
-@AllArgsConstructor
-public class EmailClientService {
-    private static final String STATUS_NOTIFICATION_TEXT = "%s %s, Your application №%s has been reviewed" +
-            " and it's status has been changed.\n" +
-            "Current status of your application: %s\n";
+public interface EmailClientService {
 
-    private static final String FROM_ADDRESS = "noreply@fintech.com";
-
-    private static final String SUBJECT = "Application status updated.";
-
-    private final EmailClient emailClient;
-
-    public void sendApplicationStatusNotification(Application application) {
-        Client client = application.getClient();
-        String messageText = String.format(STATUS_NOTIFICATION_TEXT, client.getFirstName(), client.getLatName(), application.getId(), application.getStatus());
-
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(FROM_ADDRESS);
-        message.setSubject(SUBJECT);
-        message.setTo(client.getEmail());
-        message.setText(messageText);
-
-        emailClient.sendSimpleMessage(message);
-    }
+    void sendApplicationStatusNotification(Application application);
 }
---
+++ origination/src/main/java/com/academy/fintech/origination/core/email/EmailClientServiceImpl.java
@@ -0,0 +1,35 @@
+package com.academy.fintech.origination.core.email;
+
+import com.academy.fintech.origination.core.service.application.db.application.Application;
+import com.academy.fintech.origination.core.service.application.db.client.Client;
+import lombok.AllArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EmailClientServiceImpl implements EmailClientService {
+    private static final String STATUS_NOTIFICATION_TEXT = "%s %s, Your application №%s has been reviewed" +
+            " and it's status has been changed.\n" +
+            "Current status of your application: %s\n";
+
+    private static final String FROM_ADDRESS = "noreply@fintech.com";
+
+    private static final String SUBJECT = "Application status updated.";
+
+    private final EmailClient emailClient;
+
+    @Override
+    public void sendApplicationStatusNotification(Application application) {
+        Client client = application.getClient();
+        String messageText = String.format(STATUS_NOTIFICATION_TEXT, client.getFirstName(), client.getLatName(), application.getId(), application.getStatus());
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(FROM_ADDRESS);
+        message.setSubject(SUBJECT);
+        message.setTo(client.getEmail());
+        message.setText(messageText);
+
+        emailClient.sendSimpleMessage(message);
+    }
+}
