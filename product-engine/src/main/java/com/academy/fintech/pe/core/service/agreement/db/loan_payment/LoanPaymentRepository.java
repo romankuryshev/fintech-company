c7f53107860e13d184ae4e764113b772e560272a
romankuryshev <romankuryshev1406@gmail.com>
2024-03-24 23:23:29 +0300

    feat: scheduling account update
    add scheduling account updater

--- product-engine/src/main/java/com/academy/fintech/pe/core/service/agreement/db/loan_payment/LoanPaymentRepository.java
+++ product-engine/src/main/java/com/academy/fintech/pe/core/service/agreement/db/loan_payment/LoanPaymentRepository.java
@@ -1,8 +1,13 @@
 package com.academy.fintech.pe.core.service.agreement.db.loan_payment;
 
+import com.academy.fintech.pe.core.service.agreement.db.payment_schedule.PaymentSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 public interface LoanPaymentRepository extends JpaRepository<LoanPayment, Integer> {
+    LoanPayment findByPaymentScheduleAndPeriodNumber(PaymentSchedule schedule, int periodNumber);
+    LoanPayment findByPaymentScheduleAndPaymentDate(PaymentSchedule schedule, LocalDate paymentDate);
 }
--- product-engine/src/main/java/com/academy/fintech/pe/core/service/agreement/db/payment_schedule/PaymentScheduleService.java
+++ product-engine/src/main/java/com/academy/fintech/pe/core/service/agreement/db/payment_schedule/PaymentScheduleService.java
@@ -1,21 +1,23 @@
 package com.academy.fintech.pe.core.service.agreement.db.payment_schedule;
 
 import com.academy.fintech.pe.core.service.agreement.db.agreement.Agreement;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 
 @Service
+@RequiredArgsConstructor
 public class PaymentScheduleService {
 
     private final PaymentScheduleRepository paymentScheduleRepository;
 
-    public PaymentScheduleService(PaymentScheduleRepository paymentScheduleRepository) {
-        this.paymentScheduleRepository = paymentScheduleRepository;
-    }
-
     public PaymentSchedule createNewSchedule(Agreement agreement) {
         int currentVersion = paymentScheduleRepository.countPaymentScheduleByAgreement(agreement);
         return paymentScheduleRepository.save(new PaymentSchedule(agreement, currentVersion + 1));
+    }
+
+    public PaymentSchedule getActualSchedule(Agreement agreement) {
+        return paymentScheduleRepository.findFirstByAgreementOrderByVersionAsc(agreement);
     }
 
     public PaymentSchedule save(PaymentSchedule schedule) {
